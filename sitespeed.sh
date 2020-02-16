bafa3fb90b1539bb8c52196ec4b0f497aedcb8c4
ryuichi1208 <ryucrosskey@gmail.com>
2020-02-16 14:14:07 +0900

    add sitespeed.sh

---
+++ sitespeed.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#
+# sitespeed.ioをjenkinsから実行する際のスクリプト
+#
+
+set -euC
+
+# 定数
+DOCKER_CMD=$(which docker)
+DOCKER_RUN_OPTS="container run --shm-size=1g --rm -v $(pwd):/sitespeed.io"
+DOCKER_IMAGE="sitespeedio/sitespeed.io"
+
+# テスト対象URL
+TEST_URL=""
+# テスト試行回数
+EXEC_ITERATION=""
+# sitespeed.ioのdockerイメージバージョン
+SITESPEED_VERSION=""
+# sitespeed.ioの実行時オプション
+SITESPEED_OPTS=""
+# sitespeed.ioのユーザアージェント
+
+function usage()
+{
+    cat <<EOS >&2
+Usage: $0 [-u テスト対象URL]
+  -v              sitespeed.ioのdockerイメージバージョン
+  -a              ユーザーエージェント
+EOS
+  exit 1
+}
+
+function paser_args()
+{
+  while getopts "u:v:a:e:h" OPT; do
+    case $OPT in
+      u) TEST_URL=${OPTARG} ;;
+      v) SITESPEED_VERSION=${OPTARG} ;;
+      a) SITESPEED_AGENT="${OPTARG}";;
+      e) EXEC_ITERATION="{OPTARG}";;
+      h) usage;;
+      ?) usage;;
+    esac
+  done
+
+  shift $((OPTIND - 1))
+
+  if [[ "${TEST_URL}" == "" ]]; then
+    usage
+  fi
+
+  EXEC_ITERATION="${EXEC_ITERATION:-3}"
+  SITESPEED_VERSION="${SITESPEED_VERSION:-latest}"
+  SITESPEED_AGENT="${SITESPEED_AGENT:-Chorme}"
+}
+
+function main()
+{
+  ${DOCKER_CMD} ${DOCKER_RUN_OPTS} ${DOCKER_IMAGE}:${SITESPEED_VERSION} \
+    ${SITESPEED_OPTS} \
+    -n ${EXEC_ITERATION} \
+    --userAgent "${SITESPEED_AGENT}" \
+    ${TEST_URL}
+}
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  paser_args "${@}"
+  main
+fi

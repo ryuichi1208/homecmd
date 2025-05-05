bbf9ff363c3e8ac42013af9cf9f5b7e409c12312
ryuichi1208 <ryucrosskey@gmail.com>
2025-05-05 16:05:52 +0900

    add

--- tmp/py/mac/test_hello.py
+++ tmp/py/mac/test_hello.py
@@ -1,5 +1,6 @@
 from unittest.mock import Mock, patch
 import hello
+import pytest
 
 #def test_my_class_my_method():
 #    my_class = hello.MyClass()
@@ -42,3 +43,40 @@ def test_side_effect(mock_method: Mock):
     lig = Lig()
     assert lig.add(1, 2) == -1
 
+import os
+from unittest.mock import patch
+class TestApp:
+    @patch('os.getcwd')
+    def test_patch(self, mock_getcwd):
+        mock_getcwd.return_value = 'mocked'
+        print(f"cwd={os.getcwd()}")
+        assert os.getcwd() == 'mocked'
+
+    def test_with(self):
+        with patch('os.getcwd') as mock:
+            mock.return_value = 'mocked-with'
+            print(f"with={os.getcwd()}")
+
+    @pytest.fixture()
+    def fixture_mock(self, mocker):
+        return mocker.patch(
+            'os.getcwd', return_value='mocked_2'
+        )
+
+    def test_fixture(self, fixture_mock):
+        assert os.getcwd() == 'mocked_2'
+
+class HTTPGet:
+    def __init__(self, url):
+        self.url = url
+
+    def get(self):
+        return f"original"
+
+
+class TestHTTPGet:
+    @patch('test_hello.HTTPGet.get')
+    def test_http_get(self, mock_get):
+        mock_get.return_value = 'mocked'
+        http_get = HTTPGet('http://example.com')
+        assert http_get.get() == 'mocked'

b8a2668dbcdf74eb68ea749e2061aa186e536a56
ryuichi1208 <ryucrosskey@gmail.com>
2025-05-14 09:51:59 +0900

    add: sample sort

--- tmp/stt/client/tools/sort.py
+++ tmp/stt/client/tools/sort.py
@@ -1,10 +1,18 @@
-from dataclasses import dataclass
+from pydantic_ai import Agent
 
-@dataclass
-class Sort:
-    num: list[int]
-    name: str = "Sort"
+# Define a very simple agent including the model to use, you can also set the model when running the agent.
+agent = Agent(
+    'google-gla:gemini-1.5-flash',
+    # Register a static system prompt using a keyword argument to the agent.
+    # For more complex dynamically-generated system prompts, see the example below.
+    system_prompt='Be concise, reply with one sentence.',
+)
 
-s = Sort([1, 2, 3])
-print(s.num)
-print(s.name)
+# Run the agent synchronously, conducting a conversation with the LLM.
+# Here the exchange should be very short: PydanticAI will send the system prompt and the user query to the LLM,
+# the model will return a text response. See below for a more complex run.
+result = agent.run_sync('Where does "hello world" come from?')
+print(result.output)
+"""
+The first known use of "hello, world" was in a 1974 textbook about the C programming language.
+"""
